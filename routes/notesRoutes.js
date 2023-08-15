5afcd92b4ed3c96f99e8f39067c6a7cd20541aa6
Mir Imad Ahmed <mirimadahmed@outlook.com>
2023-08-16 02:38:14 +0800

    formatitng

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -3,15 +3,15 @@ const router = express.Router()
 
 const NotesController = require('../controllers/NotesController')
 const authMiddleware = require('../middlewares/authenticate')
-const { createNotesValidationRules, updateNotesValidationRules } = require('../middlewares/notesValidation')
-const NotesController = new NotesController()
+const { createNotesValidationRules, updateNotesValidationRules, deleteNotesValidationRules } = require('../middlewares/notesValidation')
+const notesController = new NotesController()
 
 // Define the route handlers
 router.use(authMiddleware);
-router.get('/', NotesController.getAllUserNotes.bind(NotesController))
-router.get('/:noteId', NotesController.getNoteById.bind(NotesController))
-router.post('/', createNotesValidationRules, NotesController.createNewNote.bind(NotesController))
-router.put('/:noteId', updateNotesValidationRules, NotesController.updateNote.bind(NotesController))
-router.delete('/:noteId', deleteNotesValidationRules, NotesController.deleteNote.bind(NotesController))
+router.get('/', notesController.getAllUserNotes.bind(NotesController))
+router.get('/:noteId', notesController.getNoteById.bind(NotesController))
+router.post('/', createNotesValidationRules, notesController.createNewNote.bind(NotesController))
+router.put('/:noteId', updateNotesValidationRules, notesController.updateNote.bind(NotesController))
+router.delete('/:noteId', deleteNotesValidationRules, notesController.deleteNote.bind(NotesController))
 
 module.exports = router
