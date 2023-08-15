53d6e8960586e02404c82c21b8a0d0f4ecd7177d
Mir Imad Ahmed <mirimadahmed@outlook.com>
2023-08-15 18:38:26 +0800

    base layer + user model

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize({
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+  dialect: 'mysql'
+})
+
+module.exports = sequelize
---
+++ models/user.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../config/database')
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  tableName: 'users' // Specify the table name
+})
+
+User.prototype.toJSON = function () {
+  const values = { ...this.get() }
+  delete values.password
+  return values
+}
+
+module.exports = User
